8b5ca87f17d1fb4b9090c7efbbc8d75b4005980b
TB-takahiro-sakaguchi <takahiro_sakaguchi@tbn.co.jp>
2022-08-07 11:35:35 +0900

    add:deno settings

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -3,5 +3,6 @@ set -eux
 # shellcheck source=./scripts/common.bash
 source "$(dirname "$0")/common.bash"
 
+/bin/bash "$CUR_DIR/setup-deno.bash"
 /bin/bash "$CUR_DIR/setup-links.bash"
 /bin/bash "$CUR_DIR/setup-zinit.bash"
